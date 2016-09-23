9b2d7fa3ed2cbca190554cb4d8716f817fd7fd3a
harmo <dtharmo@gmail.com>
2016-09-23 22:53:09 +0200

    Add base files with gulp task and gitignore

---
+++ README.md
@@ -0,0 +1,15 @@
+# gulp-screeps-submodules
+
+A Gulp plugin for commiting code and submodules to your Screeps account.
+
+Based from [gulp-screeps](https://github.com/pcmulder/gulp-screeps)
+with subdirectories support and livereload task.
+
+## Usage
+Duplicate the ```credentials.dist.js``` to ```credentials.js``` and fill it with your screeps account informations.
+
+Place your code in ```src/``` directory
+
+Run ```gulp```
+
+Any change on your code will automatically push your files to the designed branch on the game.
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var screeps = require('gulp-screeps');
+var credentials = require('./credentials.js');
+var livereload = require('gulp-livereload');
+var rename = require('gulp-rename');
+
+gulp.task('screeps', function() {
+    gulp.src(['src/*.js', 'src/**/*.js'])
+        .pipe(rename(function(path){
+            if(path.dirname != '.'){
+                var path_array = path.dirname.split('/');
+                var new_path = '';
+                for(var i in path_array){
+                    if(path_array[i] != 'main'){
+                        new_path += (new_path.length ? '.' : '') + path_array[i];
+                    }
+                }
+                path.basename = new_path + '.' + path.basename;
+                path.dirname = '.';
+            }
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(screeps(credentials))
+        .pipe(livereload())
+});
+
+
+gulp.task('submodules', function() {
+});
+
+
+gulp.task('default', function() {
+    livereload.listen();
+    gulp.watch(['src/*.js','src/**/*.js'], ['screeps']);
+});
